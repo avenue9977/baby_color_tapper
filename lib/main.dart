b6d24c5a6225b4b863c61a129a7b0441850fec91
Nikola Chetelyazov <nikola@Nikolas-MacBook-Pro.local>
2020-04-17 18:43:11 +0300

    Added random color generation on display tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:random_color/random_color.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: MyHomePage(),
     );
   }
@@ -31,27 +21,45 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  static RandomColor _randomColor = RandomColor();
+
+  Color _color = Colors.yellow;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    backgroundColor: Colors.deepPurple,
-    body: Center(
-      child: Text('You have pressed the button $_counter times.'),
-    ),
-    bottomNavigationBar: BottomAppBar(
-      shape: const CircularNotchedRectangle(),
-      child: Container(height: 60.0,),
-    ),
-    floatingActionButton: FloatingActionButton(
-      backgroundColor: Colors.yellow[700],
-      onPressed: () => setState(() {
-        _counter++;
-      }),
-      child: Icon(Icons.lock_open),
-    ),
-    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-  );
+      backgroundColor: _color,
+      body: GestureDetector(
+        onTap: () {
+          setState(() {
+            _color = _randomColor.randomColor(
+                colorSaturation: ColorSaturation.highSaturation,
+                colorBrightness: ColorBrightness.dark
+              );
+          });
+        },
+        child: Container(
+          color: _color,
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: Colors.deepOrange[700],
+        child: Container(
+          height: 60.0,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.yellow[700],
+        onPressed: () => setState(() {
+          // TODO Implement logic for locking the display
+        }),
+        child: Icon(
+          Icons.lock_open,
+          color: Colors.black,
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
   }
 }
